dea4e34c3dc0d9b0077fad761cf83eb23df557a8
Vanapala Akash Varma <akashvarmav2910@gmail.com>
2025-10-31 09:04:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3238265792-2637c",
+  "appId": "1:929467774901:web:dffb1f6e0ac3684b21cbfc",
+  "apiKey": "AIzaSyCSeuYshlC8DDiMy8wzrDiRX_1Np24b2SI",
+  "authDomain": "studio-3238265792-2637c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "929467774901"
+};
